5671c90a4d0ee4f77e12b1685ab73a39cfe3eccf
Matthew Brown <brownm18@rpi.edu>
2019-03-11 15:11:46 -0400

    Project 1A

---
+++ Assets/Scripts/SFXManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SFXManager : MonoBehaviour {
+    /// <summary>
+    /// This script manages the background music as well as the 
+    /// sound made when an apple is caught.
+    /// </summary>
+
+    [Header("Background")]
+    public AudioSource backgroundSource;    // Source of background music
+    [Range(0, 1)]
+    public float backgroundVolume;          // Volume of background music
+
+    [Header("Catching Sound Effect")]
+    public AudioSource cacthingSource;      // Source of cacthing sound
+    public AudioClip cacthingClip;          // Clip of cacthing sound
+    [Range(0, 1)]
+    public float cacthingVolume;            // Volume of cacthing sound
+
+    private static System.Boolean caught = false;
+
+    public void Start()
+    {
+        backgroundSource.Play();            // Play background music
+    }
+
+    public void Update()
+    {
+        cacthingSource.volume = cacthingVolume; // Change button volume
+        backgroundSource.volume = backgroundVolume; // Change background volume
+        if (caught)
+        {
+            cacthingSource.PlayOneShot(cacthingClip); // Play button sound
+            caught = false;
+        }
+    }
+    public static void Catch()
+    {
+        caught = true;
+    }
+}
---
+++ Assets/Scripts/Tree_Movement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tree_Movement : MonoBehaviour {
+    /// <summary>
+    /// Manages the Tree's movement
+    /// </summary>
+    private float directionChangeChance = .05f; // Chance of the tree changing directions
+    private float boarder = 7.5f;               // Edge of the Screen
+    private int direction = 1;                  // Direction the tree is traveling
+
+    private void FixedUpdate()
+    {
+        float speed = Random.Range(0, .2f);     // Spead of the tree
+        if (Random.value <= directionChangeChance || transform.position.x > boarder || transform.position.x < (-1* boarder))
+        {
+            direction *= -1;
+        }
+        float new_x = transform.position.x + (speed*direction); // New x location of the tree
+        if (new_x > boarder)
+        {                                       // Tree is too far right
+            new_x = boarder;
+        }else if (new_x < (-1* boarder))
+        {                                       // Tree is too far left
+            new_x = (-1 * boarder);
+        }
+        transform.position = new Vector3(new_x, 3f, 0); // Move tree
+    }
+}
