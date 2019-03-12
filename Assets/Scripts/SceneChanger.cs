e3c307cad1044fdd9fd3d2d5cf2bab4a8a7b612f
Matthew Brown <brownm18@rpi.edu>
2019-03-12 10:56:21 -0400

    Adding Scenes

---
+++ Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+
+    public void ChangeScene(string sceneName)
+    {
+
+        if (sceneName == "" || sceneName == null)
+        {
+            Debug.LogWarning("Empty Scene Name");
+        }
+        else if (sceneName == "Quit")
+        {
+            Application.Quit();
+        }
+        else
+        {
+            Debug.Log(sceneName);
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+}
