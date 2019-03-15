f9a724cdab035069c5a81935f3e638989e79445a
Matthew Brown <brownm18@rpi.edu>
2019-03-15 01:20:30 -0400

    Fixing Github Mistakes

--- Assets/Scripts/SFXManager.cs
+++ Assets/Scripts/SFXManager.cs
@@ -7,37 +7,90 @@ public class SFXManager : MonoBehaviour {
     /// This script manages the background music as well as the 
     /// sound made when an apple is caught.
     /// </summary>
+ 
+    public static SFXManager instance = null; // Instance of this class
 
     [Header("Background")]
-    public AudioSource backgroundSource;    // Source of background music
+    public AudioSource backgroundSource;    // Music Sourse
     [Range(0, 1)]
-    public float backgroundVolume;          // Volume of background music
+    public float backgroundVolume;          // Music Volume
 
     [Header("Catching Sound Effect")]
-    public AudioSource cacthingSource;      // Source of cacthing sound
-    public AudioClip cacthingClip;          // Clip of cacthing sound
+    public AudioSource cacthingSource;      // Effect Sourse
+    public AudioClip cacthingClip;          // Effect Clip
     [Range(0, 1)]
-    public float cacthingVolume;            // Volume of cacthing sound
+    public float cacthingVolume;            // Effect Volume
 
-    private static System.Boolean caught = false;
-
-    public void Start()
+    public void Awake()
     {
-        backgroundSource.Play();            // Play background music
+        // Creates instance of this class
+        if (instance == null){ instance = this; }
+        else{ Destroy(gameObject); }
+
+        // Creates player volume preferences (Music)
+        if (!PlayerPrefs.HasKey("Background Volume"))
+        {
+            PlayerPrefs.SetFloat("Background Volume", backgroundVolume); // Defualt Value
+        }
+        instance.backgroundVolume = PlayerPrefs.GetFloat("Background Volume"); // Get past perference
+        instance.backgroundSource.volume = instance.backgroundVolume; // Set Music Volume
+
+
+        // Creates player volume preferences (Effects)
+        if (!PlayerPrefs.HasKey("Effect Volume"))
+        {
+            PlayerPrefs.SetFloat("Effect Volume", cacthingVolume); // Defualt Value
+        }
+        instance.cacthingVolume = PlayerPrefs.GetFloat("Effect Volume");  // Get past perference
+        instance.cacthingSource.volume = instance.cacthingVolume; // Set Effect Volume
     }
 
-    public void Update()
+    // Increaing the Music Volume
+    public void BackgroundVolumeUp()
     {
-        cacthingSource.volume = cacthingVolume; // Change button volume
-        backgroundSource.volume = backgroundVolume; // Change background volume
-        if (caught)
+        if (instance.backgroundVolume + .1f <= 1.1)
+        { 
+            instance.backgroundVolume += .1f; // Increase 
+            PlayerPrefs.SetFloat("Background Volume", instance.backgroundVolume); // Set player preference
+            instance.backgroundSource.volume = instance.backgroundVolume; // Set Music Volume
+        }
+
+    }
+    // Decreasing the Music Volume
+    public void BackgroundVolumeDown()
+    {
+        if (instance.backgroundVolume - .1f >= 0)
         {
-            cacthingSource.PlayOneShot(cacthingClip); // Play button sound
-            caught = false;
+            instance.backgroundVolume -= .1f;
+            PlayerPrefs.SetFloat("Background Volume", instance.backgroundVolume); // Set player preference
+            instance.backgroundSource.volume = instance.backgroundVolume; // Set Music Volume
         }
     }
-    public static void Catch()
+    // Increaing the Effect Volume
+    public void EffectVolumeUp()
     {
-        caught = true;
+        if (instance.cacthingVolume + .1f <= 1.1)
+        {
+            instance.cacthingVolume += .1f; 
+            PlayerPrefs.SetFloat("Effect Volume", instance.cacthingVolume); // Set player preference
+            instance.cacthingSource.volume = instance.cacthingVolume; // Set Effect Volume
+        }
+
+    }
+    // DEcreasing the Effect Volume
+    public void EffectVolumeDown()
+    {
+        if (instance.cacthingVolume - .1f >= 0)
+        {
+            instance.cacthingVolume -= .1f;
+            PlayerPrefs.SetFloat("Effect Volume", instance.cacthingVolume); // Set player preference
+            instance.cacthingSource.volume = instance.cacthingVolume; // Set Effect Volume
+        }
+
+    }
+    // Sound of catching an apple
+    public void Catch()
+    {
+        cacthingSource.PlayOneShot(cacthingClip);
     }
 }
